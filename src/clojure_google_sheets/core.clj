5f9929e6ebecd76c51704ccc038026847247be07
pvgomes <paulo.gomes@nubank.com.br>
2020-09-24 21:18:36 -0300

    define central core start point

--- src/clojure_google_sheets/core.clj
+++ src/clojure_google_sheets/core.clj
@@ -1,18 +1,15 @@
 (ns clojure-google-sheets.core
   (:gen-class)
-  (:require [clojure.java.io :as io])
+  (:require [clojure-google-sheets.config :as config]
+            [clojure-google-sheets.sheets-v4 :as sheets-v4])
   (:import (com.google.api.client.extensions.java6.auth.oauth2 AuthorizationCodeInstalledApp)
-           (com.google.api.client.extensions.jetty.auth.oauth2 LocalServerReceiver$Builder)
            (com.google.api.client.googleapis.auth.oauth2 GoogleClientSecrets GoogleAuthorizationCodeFlow$Builder)
            (com.google.api.client.googleapis.javanet GoogleNetHttpTransport)
            (com.google.api.client.json.jackson2 JacksonFactory)
-           (com.google.api.client.util.store FileDataStoreFactory DataStoreFactory)
-           (com.google.api.services.sheets.v4 Sheets$Builder SheetsScopes Sheets)
-           (com.google.api.services.sheets.v4.model ValueRange)))
+           (com.google.api.client.util.store DataStoreFactory)
+           (com.google.api.services.sheets.v4 Sheets$Builder)))
 
-(set! *warn-on-reflection* true)
-
-(defn gservice
+(defn google-service
   [{::keys [^String application-name ^DataStoreFactory tokens-directory
             credentials authorize json-factory scopes access-type port]}]
   (let [http-transport (GoogleNetHttpTransport/newTrustedTransport)
@@ -25,9 +22,7 @@
                  (.setDataStoreFactory tokens-directory)
                  (.setAccessType access-type)
                  .build)
-        receiver (-> (LocalServerReceiver$Builder.)
-                     (.setPort port)
-                     .build)]
+        receiver (sheets-v4/receiver port)]
     (-> (Sheets$Builder.
           http-transport json-factory
           (-> (AuthorizationCodeInstalledApp. flow receiver)
@@ -35,30 +30,17 @@
         (.setApplicationName application-name)
         .build)))
 
-(defn get-values
-  [^Sheets gservice id range]
-  (let [^ValueRange response (-> gservice
-                                 .spreadsheets
-                                 .values
-                                 (.get id range)
-                                 .execute)]
-    (.getValues response)))
-
 (defn -main
   [& _]
-  (let [service (gservice {::application-name "Google Sheets API Java Quickstart"
+  (let [service (google-service {::application-name "Google Sheets API Java Quickstart"
                            ::access-type      "offline"
                            ::port             8888
                            ::authorize        "user"
-                           ::credentials      (-> "credentials.json"
-                                                  io/resource
-                                                  io/reader)
-                           ::tokens-directory (-> "tokens"
-                                                  io/file
-                                                  FileDataStoreFactory.)
-                           ::scopes           [SheetsScopes/SPREADSHEETS_READONLY]
+                           ::credentials      (config/credentials)
+                           ::tokens-directory (config/tokens)
+                           ::scopes           config/scopes
                            ::json-factory     (JacksonFactory/getDefaultInstance)})
-        values (get-values service
+        values (sheets-v4/get-values service
                            "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
                            "Class Data!A2:E")]
     (if (empty? values)
