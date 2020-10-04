da7482f0e3b143a17c09a3b92b2d5bba4fe84171
pvgomes <paulo.gomes@nubank.com.br>
2020-10-04 14:13:46 -0300

    update examples and models

--- src/clojure_google_sheets/sheets_v4.clj
+++ src/clojure_google_sheets/sheets_v4.clj
@@ -1,33 +1,17 @@
 (ns clojure-google-sheets.sheets-v4
-  (:require [clj-time.core :as time]
-            [clojure-google-sheets.config :as config])
+  (:require
+    [schema.core :as s])
   (:import (com.google.api.client.extensions.jetty.auth.oauth2 LocalServerReceiver$Builder)
            (com.google.api.services.sheets.v4 Sheets)
            (com.google.api.services.sheets.v4.model ValueRange
-                                                    AddSheetRequest
-                                                    AppendCellsRequest
                                                     BatchUpdateSpreadsheetRequest
                                                     CellData
-                                                    CellFormat
-                                                    DeleteDimensionRequest
-                                                    DimensionRange
                                                     ExtendedValue
                                                     GridCoordinate
-                                                    GridProperties
-                                                    InsertDimensionRequest
-                                                    NumberFormat
                                                     Request
                                                     RowData
-                                                    SheetProperties
                                                     UpdateCellsRequest
-                                                    UpdateSheetPropertiesRequest)
-           (com.google.api.client.json.jackson2 JacksonFactory)
-           (com.google.api.client.extensions.java6.auth.oauth2 AuthorizationCodeInstalledApp)
-           (com.google.api.client.googleapis.auth.oauth2 GoogleClientSecrets GoogleAuthorizationCodeFlow$Builder)
-           (com.google.api.client.googleapis.javanet GoogleNetHttpTransport)
-           (com.google.api.client.json.jackson2 JacksonFactory)
-           (com.google.api.client.util.store DataStoreFactory)
-           (com.google.api.services.sheets.v4 Sheets$Builder)))
+                                                    BatchUpdateSpreadsheetResponse)))
 
 (set! *warn-on-reflection* true)
 
@@ -54,8 +38,8 @@
                               (-> (ExtendedValue.)
                                   (.setStringValue %)))) row))))
 
-(defn write-values
-  [^Sheets gservice id range values]
+(s/defn write-values :- BatchUpdateSpreadsheetResponse
+  [^Sheets gservice id sheet-id row-index values]
   (let [write-request (-> (Request.)
                           (.setUpdateCells
                             (-> (UpdateCellsRequest.)
@@ -64,9 +48,9 @@
                                       ;sheet id is a range on a sheet. All indexes are zero-based. it means that a first sheet is 0,
                                       ;Easiest way to find the sheet id is in the browser URL when you open the spreadsheet / sheet: https://docs.google.com/spreadsheets/d/{spreadsheetId}/edit#gid={sheetId}
                                       ;more info see here https://developers.google.com/resources/api-libraries/documentation/sheets/v4/csharp/latest/classGoogle_1_1Apis_1_1Sheets_1_1v4_1_1Data_1_1GridRange.html
-                                      (.setSheetId (int 271526484))
+                                      (.setSheetId (int sheet-id))
                                       ;1 = 0 (first row)
-                                      (.setRowIndex (int 2))
+                                      (.setRowIndex (int row-index))
                                       ;A = 0 (first column)
                                       (.setColumnIndex (int 0))))
                                 (.setRows [(row->row-data values)])
@@ -77,54 +61,4 @@
                            id
                            (-> (BatchUpdateSpreadsheetRequest.)
                                   (.setRequests [write-request])))
-                         .execute)
-))
-
-(defn google-service
-  [{::keys [^String application-name ^DataStoreFactory tokens-directory
-            credentials authorize json-factory scopes access-type port]}]
-  (let [http-transport (GoogleNetHttpTransport/newTrustedTransport)
-        client-secrets (GoogleClientSecrets/load json-factory credentials)
-        flow (-> (GoogleAuthorizationCodeFlow$Builder.
-                   http-transport
-                   json-factory
-                   client-secrets
-                   scopes)
-                 (.setDataStoreFactory tokens-directory)
-                 (.setAccessType access-type)
-                 .build)
-        receiver (receiver port)]
-    (-> (Sheets$Builder.
-          http-transport json-factory
-          (-> (AuthorizationCodeInstalledApp. flow receiver)
-              (.authorize authorize)))
-        (.setApplicationName application-name)
-        .build)))
-
-(comment
-
-
-  (let [service (google-service {::application-name "Google Sheets API Java Quickstart"
-                                 ::access-type      "offline"
-                                 ::port             8888
-                                 ::authorize        "user"
-                                 ::credentials      (config/credentials)
-                                 ::tokens-directory (config/tokens)
-                                 ::scopes           config/scopes
-                                 ::json-factory     (JacksonFactory/getDefaultInstance)})
-        ;values (get-values service
-        ;                   (:spreadsheet-id (config/sheet-config))
-        ;                   "Sheet1!A2:E")
-        values (write-values service
-                             (:spreadsheet-id (config/sheet-config))
-                             "Sheet2!A2:E"
-                             (java.util.ArrayList. ["POMO3", "10/04/2020", "100", "2.56", "256", "2.56", "0","0"]))
-
-        ]
-    (println values))
-
-  )
-
-
-
-
+                         .execute)))
